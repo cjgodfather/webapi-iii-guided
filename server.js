86146bc08ce6335e148eaee065f6cc437c3c7db5
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-30 13:29:47 -0400

    gateKeeper practice

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express"); // importing a CommonJS module
 const helmet = require("helmet");
+const morgan = require("morgan");
 const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
@@ -10,17 +11,30 @@ function dateLogger(req, res, next) {
 }
 
 function customLogger(req, res, next) {
-  const httpLoc = req.originalUrl;
+  const httpLoc = req.url;
   const method = req.method;
-  console.log(`${httpLoc}${method}`);
+  console.log(`${method}${httpLoc}`);
   next();
 }
 
+function gateKeeper(req, res, next) {
+  const password = req.headers.password;
+  if (!password) {
+    res.status(400).json({ message: "provide a password" });
+  } else if (password.toLowerCase() === "mellon") {
+    next();
+  } else {
+    res.status(400).json({ message: "cannot pass !" });
+  }
+}
+
 // global middleware
+server.use(gateKeeper);
 server.use(helmet());
 server.use(express.json());
 server.use(dateLogger);
 server.use(customLogger);
+server.use(morgan("dev"));
 
 server.use("/api/hubs", hubsRouter);
 
